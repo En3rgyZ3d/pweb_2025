e1340df7a6ad0307722776297fe4a2d2640cdb30
en3rgyz3d <jekivilmigliore@gmail.com>
2025-04-30 11:57:06 +0200

    Added frontend.py and more html templates
    Added frontend.py and more html templates

--- app/main.py
+++ app/main.py
@@ -1,19 +1,12 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from .routers import books
+from starlette.staticfiles import StaticFiles
 
+from .routers import books, frontend
 app = FastAPI()
 app.include_router(books.router, tags=["books"]) #il campo tags riporta il nome dell'endpoint (serve per la docs)
 
-templates = Jinja2Templates(directory="app/templates")
-
-@app.get("/", response_class=HTMLResponse, tags=["HTML Responses"])
-def home(request: Request): # Aggiungiamo il parametro alla funzione in modo che la funzione riceva la richiesta http
-    '''Returns the home page.'''
-
-
-    # Ovviamente lavorare con python in stringhe è abominevole, quindi si restituiscono i file HTML nella cartella templates.
-    return templates.TemplateResponse(
-        name = "home.html", request =request
-    )
+app.include_router(frontend.router, tags=["frontend"])
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# il primo parametro è l'url della web app dove possono essere recuperate le risorse
+# il secondo è il percorso della cartella dove si possono recuperare i file
+# il terzo è un tag identificativo
---
+++ app/routers/frontend.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from ..data.books import books
+
+router = APIRouter() # Parte dal root principale, quindi non mettiamo i prefix
+
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/", response_class=HTMLResponse, tags=["HTML Responses"])
+def home(request: Request): # Aggiungiamo il parametro alla funzione in modo che la funzione riceva la richiesta http
+    '''Returns the home page.'''
+
+    context = {'title': "Welcome to the library"}
+    # Se al posto della stringa metto dei tag HTML, questi vengono convertiti in stringa e non vengono interpretati come tag.
+    # Ciò che avviene si chiama "escaping". Avviene perché se non fosse così, basterebbe usare i tag script ed eseguire codice arbitrario.
+    # L'escaping è attivato di base su Jinja2, nel caso si voglia rendere esplicito, basterà mettere nell'HTML la variabile con l'or
+    # Ad esempio {{text|e}}. E' consigliato fare l'operazione esplicita quando si ottengono input dall'esterno.
+
+    context_dict = {'title': "Welcome to the library",
+                    'content' : "Hello!"}
+
+    #Se invece mandiamo un dizionario, il dizionario viene convertito in stringa.
+
+
+    # Ovviamente lavorare con python in stringhe è abominevole, quindi si restituiscono i file HTML nella cartella templates.
+    return templates.TemplateResponse(
+        name = "home.html", request =request, #Sarà jinja2 ad occuparsi di estendere i blocchi
+        context = {"text": context_dict} #Serve a passare i parametri per essere renderizzati sull'HTML | Context prende un dizionario avente come chiavi i nomi delle variabili
+    )
+
+@router.get("/book_list", response_class=HTMLResponse, tags=["HTML Responses"])
+def show_book_list(request: Request):
+    '''Returns the book list page.'''
+
+    context = list(books.values())
+
+    return templates.TemplateResponse(
+        name="list.html", request =request,
+        context={"books": context}
+    )
